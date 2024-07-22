d0f6de036c146776e796966c431b16fe5b5c3485
CheckEZ <hihilolrr@gmail.com>
2024-07-22 15:18:07 +0300

    update search

---
+++ blog/static/js/search.js
@@ -0,0 +1,10 @@
+var form_search = document.querySelector('#form_search');
+
+form_search.addEventListener('submit', send_search);
+
+async function send_search(e) {
+    e.preventDefault();
+    var search = document.querySelector('input[name="search"]');
+    var url = window.location.protocol + '//' + window.location.host + '/search/' + search.value + '/';
+    window.location.replace(url);
+}
---
+++ blog/static/js/search_tags.js
@@ -0,0 +1,10 @@
+var form_search_tags = document.querySelector('#form_search_tags');
+
+form_search_tags.addEventListener('submit', send_search_tags);
+
+async function send_search_tags(e) {
+    e.preventDefault();
+    var search_tags = document.querySelector('input[name="search_tags"]');
+    var url = window.location.protocol + '//' + window.location.host + '/search_tags/' + search_tags.value + '/';
+    window.location.replace(url);
+}
