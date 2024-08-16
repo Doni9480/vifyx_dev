804c845fa212883b352cab955712b9e3d4155fe3
vitekes <vitekes@gmail.com>
2024-08-16 10:13:28 +0300

    Add albums to config

--- blog/albums/admin.py
+++ blog/albums/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 from albums.models import Album, AlbumView, Category, Subcategory
-
 
 admin.site.register(Album)
 admin.site.register(AlbumView)
 admin.site.register(Category)
 admin.site.register(Subcategory)
+
+
+
+@admin.register(Album)
+class AlbumAdmin(ModelAdmin):
+    pass
+
+@admin.register(Category)
+class AlbumAdmin(ModelAdmin):
+    pass
+
+@admin.register(Subcategory)
+class AlbumAdmin(ModelAdmin):
+    pass
+
+@admin.register(AlbumView)
+class AlbumAdmin(ModelAdmin):
+    pass
