49ec5c9b9484b8dd62d3c94dcecec17f57002498
Daniil <daniilmirlanuulu@gmail.com>
2024-08-22 02:35:38 +0600

    fix

--- blog/campaign/api/urls.py
+++ blog/campaign/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from blog.routers import CustomRouter
 from rest_framework.routers import DefaultRouter
-from .views import CampaignViewSet, TaskTypesViewSet, TaskCheckingViewSet
+from .views import CampaignViewSet, TaskTypesViewSet, TaskCheckingViewSet, SubscriptionsCampaignViewSet
 
 router = CustomRouter()
 router.register(r'', CampaignViewSet, basename='campaign')
+router.register(r'', SubscriptionsCampaignViewSet, basename='subscriptions_campaign')
 router.register(r'tasks', TaskTypesViewSet, basename='tasks')
 router.register(r'', TaskCheckingViewSet, basename='tasks-checking')
 
